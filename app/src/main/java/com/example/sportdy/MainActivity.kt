e085dadf0f06fd73ca50c17b9d4cb64c3ff43650
liow122 <liowzm-wm18@student.tarc.edu.my>
2020-01-17 12:56:57 +0800

    can login, can register

--- app/src/main/java/com/example/sportdy/MainActivity.kt
+++ app/src/main/java/com/example/sportdy/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.sportdy
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -10,15 +19,14 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.sportdy.Login.LoginActivity
 import com.example.sportdy.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.example.sportdy.Game.GameFragment
+import kotlinx.android.synthetic.main.activity_login.*
+
+import kotlinx.android.synthetic.main.activity_register.*
 
 
 class MainActivity : AppCompatActivity(), DrawerLocker {
@@ -26,7 +34,7 @@ class MainActivity : AppCompatActivity(), DrawerLocker {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var navController: NavController
-
+    private val sharedPrefFile = "users"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
@@ -66,7 +74,8 @@ class MainActivity : AppCompatActivity(), DrawerLocker {
                 R.id.communityFragment,
                 R.id.settingsFragment,
                 R.id.aboutUsFragment,
-                R.id.helpFragment
+                R.id.helpFragment,
+                R.id.logoutId
             ), drawerLayout
         )
 
@@ -75,6 +84,15 @@ class MainActivity : AppCompatActivity(), DrawerLocker {
 //
 //        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 //        NavigationUI.setupWithNavController(binding.navView,navController)
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
+            Context.MODE_PRIVATE)
+        val sharedNameValue = sharedPreferences.getString("name_key","name")
+
+
+        val navigationView : NavigationView =findViewById(R.id.navView)
+        val headerView : View = navigationView.getHeaderView(0)
+        val navHeaderName = headerView.findViewById<TextView>(R.id.navHeaderName)
+        navHeaderName.text=sharedNameValue
     }
 
     override fun onBackPressed() {
@@ -84,6 +102,7 @@ class MainActivity : AppCompatActivity(), DrawerLocker {
             super.onBackPressed()
         }
     }
+
 
     override fun setDrawerEnabled(enabled: Boolean) {
         val lockMode = if (enabled)
@@ -97,27 +116,34 @@ class MainActivity : AppCompatActivity(), DrawerLocker {
         drawerLayout.setDrawerLockMode(lockMode)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logoutId){
+            val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
+                Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor =  sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }else{
+            return super.onOptionsItemSelected(item)
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+
 //    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        val fragment = when(item.itemId) {
-//            R.id.gameFragment ->
-//                GameFragment()
-//            R.id.chatFragment ->
-//                ChatFragment()
-//            R.id.friendFragment ->
-//                FriendFragment()
-//            R.id.communityFragment ->
-//                CommunityFragment()
-//            R.id.settingsFragment ->
-//                SettingsFragment()
-//            R.id.aboutUsFragment ->
-//                AboutUsFragment()
-//            R.id.helpFragment ->
-//                HelpFragment()
-//            else ->
-//                GameFragment()
-//        }
+//        if(item.itemId == R.id.logoutId){
+//            val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
+//                Context.MODE_PRIVATE)
+//            val editor: SharedPreferences.Editor =  sharedPreferences.edit()
+//            editor.clear()
+//            editor.apply()
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
 //
-//        supportFragmentManager.beginTransaction().replace(R.id.mainHostFragment, fragment).commit()
+//        }
 //        drawerLayout.closeDrawer(GravityCompat.START)
 //        return true
 //    }
